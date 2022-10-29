21562442bdb68fb26fcc549d07c5b09bef145729
sqwuilo <serezhaonufrienko@yandex.ru>
2022-10-29 17:48:34 +0300

    G

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        numDes(num);
+
+
+    }
+    public static void numDes(int a){
+        a = a / 10;
+        System.out.println(a);
+    }
+}
